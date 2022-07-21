71730ab50ca153ab99061ab570e3a2545db559cb
emmapr123 <emmapriester@hotmail.com>
2022-07-21 11:47:48 +0100

    Fixed broken file path

--- src/reviews/review.module.ts
+++ src/reviews/review.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { PubModule } from '../../src/pubs/pub.module';
-import { UserModule } from '../../src/users/user.module';
+import { PubModule } from '../pubs/pub.module';
+import { UserModule } from '../users/user.module';
 import { Review } from './entities/review.entity';
 import { ReviewController } from './review.controller';
 import { ReviewService } from './review.service';
